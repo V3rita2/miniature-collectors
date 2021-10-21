add1a7bc9975c5eca4b0252aec7a37b7a874f13c
Veritas <rose.daemon.belmont@gmail.com>
2021-10-21 16:02:18 -0700

    full crud

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('about/', views.About.as_view(), name='about'),
     path('armies/new/', views.ArmyCreate.as_view(), name='army_create'),
     path('army/<int:pk>/', views.ArmyDetail.as_view(), name="army_detail"),
+    path('army/<int:pk>/update', views.ArmyUpdate.as_view(), name='army_update'),
+    path('army/<int:pk>/delete', views.ArmyDelete.as_view(), name='army_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, reverse
 from django.views import View
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import DetailView
 # models imports
 from .models import Army
@@ -35,16 +35,30 @@ class ArmyCreate(CreateView):
     fields = ['name', 'image', 'desc']
     template_name = "army_create.html"
 
-    def form_valid(self, form):
-        # form.instance.user = self.request.user
-        return super(ArmyCreate, self).form_valid(form)
+    # def form_valid(self, form):
+    #     # form.instance.user = self.request.user
+    #     return super(ArmyCreate, self).form_valid(form)
 
-    # def get_success_url(self):
-    #     print(self.kwargs)
-    #     return reverse('armies_list', kwargs={'pk': self.object.pk})
+    def get_success_url(self):
+        print(self.kwargs)
+        return reverse('armies_detail', kwargs={'pk': self.object.pk})
 
     success_url = "/armies/"
 
 class ArmyDetail(DetailView):
     model = Army
     template_name = "army_detail.html"
+
+class ArmyUpdate(UpdateView):
+    model = Army
+    fields = ['name', 'image', 'desc']
+    template_name = "army_update.html"
+    success_url = '/armies/'
+
+    def get_success_url(self):
+        return reverse('army_detail', kwargs={'pk': self.object.pk})
+
+class ArmyDelete(DeleteView):
+    model = Army
+    template_name = 'army_delete_confirm.html'
+    success_url = "/armies/"
